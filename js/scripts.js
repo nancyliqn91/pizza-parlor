21e2fab5478a6bcc32c72e4a19b7d4cd9fbb468d
nancyliqn91 <nancyliqn91@gmail.com>
2023-04-22 23:05:05 -0700

    Add reset button

--- js/scripts.js
+++ js/scripts.js
@@ -39,14 +39,13 @@
     const toppings = toppingFunction();
     const sizeInput = document.querySelector("input[name='size']:checked");
     const size = sizeInput ? parseInt(sizeInput.value) : null;
-    const ordersForm = document.querySelector("form#orders");
     const address = document.querySelector('#location').value;
     
     let pizzas = [];
     const newPizza = new Pizza(toppings, size, 'order1', pizzas.length + 1);
     pizzas.push(newPizza);
-  
-    let priceTotal = 0;
+    console.log(pizzas);
+    
     const pizzaListElement = document.querySelector(".pizza-list");
     pizzaListElement.innerHTML = ""; 
     pizzas.forEach(function(pizza) {
@@ -55,19 +54,30 @@
       pizzaLink.href = "#"; 
       pizzaLink.textContent = `Pizza #${pizza.id}`; 
       pizzaLink.addEventListener("click", function() {
-      window.alert("Toppings:", pizza.toppings. "Size:", pizza.size);
+      window.alert("Toppings:", pizza.toppings);
       });
       pizzaElement.appendChild(pizzaLink);
       pizzaListElement.appendChild(pizzaElement);
+      // let priceTotal = 0;
+      // priceTotal += newPizza.addPrice(); 
+      // console.log(priceTotal);
+    });
+
+    let priceTotal = 0;
+    pizzas.forEach(function(pizza) {
       priceTotal += pizza.addPrice(); 
-      return priceTotal;
     });
 
     document.querySelector(".total-price").innerText = `Price is ${priceTotal} dollars. ${address}.`; 
-
-    ordersForm.reset();
   }
   
+  function handleConfirm(event) {
+    event.preventDefault();
+    const ordersForm = document.querySelector("form#orders");
+    ordersForm.reset();
+  }
+
   window.addEventListener("load", function() {
     document.querySelector("form#orders").addEventListener("submit", handleSubmission);
+    document.getElementById("confirm").addEventListener("click", handleConfirm); 
   });
